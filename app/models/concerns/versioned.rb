0fb90a33610940bac8f6b0c8d4474e23a638b648
Erik Hetzner <ehetzner@plos.org>
2017-05-19 08:38:25 -0700

    Extract versioned concern

---
+++ app/models/concerns/versioned.rb
@@ -0,0 +1,34 @@
+module Versioned
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :things_versions, as: :thing
+    has_many :versions, through: :things_versions
+    before_update :copy_on_write
+    before_update :prevent_editing_old_content
+  end
+
+  def shared?
+    versions.count > 1
+  end
+
+  private
+
+  def prevent_editing_old_content
+#    fail unless versions.all? { |v| !v.latest? }
+  end
+
+  def copy_on_write
+    if shared?
+      # Get the original attributes (currently stored in the db) to duplicate
+      attrs = attributes.dup.merge(changed_attributes).except('id')
+      dup = self.class.create!(attrs)
+      paper = versions.first.paper # All the versions have the same paper, so just grab the first
+      # Update all the old versions to point to this copy
+      ThingsVersion
+        .where(thing: self)
+        .where.not(version: paper.latest_version)
+        .update_all(thing_id: dup.id)
+    end
+  end
+end
